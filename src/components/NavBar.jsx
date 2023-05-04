97b6afb6b2f8e2e05bee5ffd58cb35b0c1648c53
GLVSKiriti <glvskiriti2003369@gmail.com>
2023-05-04 13:14:50 +0530

    Project Intialized

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60px;
+  background-color: black;
+`;
+
+function NavBar() {
+  return <Container>This is Nav Bar</Container>;
+}
+
+export default NavBar;
