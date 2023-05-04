95169b22a2beebba7a45e232b9f7c8fdc439012b
GLVSKiriti <glvskiriti2003369@gmail.com>
2023-05-04 15:08:29 +0530

    NavBar is created and Announcement also

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,34 @@
 import React from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  height: 60px;
-  background-color: black;
-`;
+import "../components/NavBar.css";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import { Badge } from "@material-ui/core";
 
 function NavBar() {
-  return <Container>This is Nav Bar</Container>;
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <div className="left">
+          <span>EN</span>
+          <div className="searchcontainer">
+            <input type="text" className="input" />
+            <Search style={{ color: "gray", fontSize: 16 }} />
+          </div>
+        </div>
+        <div className="center">
+          <h1 className="logo">TrendZ</h1>
+        </div>
+        <div className="right">
+          <div className="menuItem">Register</div>
+          <div className="menuItem">SignIn</div>
+          <div className="menuItem">
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
